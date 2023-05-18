aa90dad7ea3c1cce5bc7f6875e0402431ac81fa7
SHARM2575A <vidhansharma026@gmail.com>
2023-05-18 18:01:24 +0530

    'conn_db_and_dynamic'

--- Ecommerce/tea/models.py
+++ Ecommerce/tea/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name  = models.CharField(max_length=255)
+    email  = models.EmailField(max_length=255)
+    subject  = models.CharField(max_length=255)
+    message = models.TextField()
+
+class Product(models.Model):
+    discription = models.TextField()
+    upload_img = models.ImageField(upload_to='product/')
--- Ecommerce/tea/views.py
+++ Ecommerce/tea/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . models import *
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    item = Product.objects.all()
+    return render(request,'home.html',{'item':item})
     
 def about(request):
     return render(request,'about.html')
 
 def contact(request):
-    return render(request,'contact.html')
+    details = Person.objects.all()
+    return render(request,'contact.html',{'details':details})
+
+def contact_us(request):
+    if request.method == 'POST':
+        name  = request.POST['name']
+        email  = request.POST['email']
+        subject  = request.POST['subject']
+        message = request.POST['message']
+        if Person.objects.filter(email = email).exists():
+            messages.error(request,'email is already exists')
+            return redirect('/contact/')
+        else:
+            Person.objects.create(name = name, email = email, subject = subject, message = message)
+            messages.success(request,'Details successfully added')
+            details = Person.objects.all()
+            return render(request,'contact.html',{'details':details})
+            
